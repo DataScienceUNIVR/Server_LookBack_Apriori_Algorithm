723d9cb62a6945417d60356ec0cd03630511743e
MarcoCastelli4 <36838250+MarcoCastelli4@users.noreply.github.com>
2022-05-04 15:26:42 +0200

    Api development

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import utilities
 from flask import Flask, render_template, redirect, url_for,request
 from flask_bootstrap import Bootstrap
 import os.path
+import json
 app = Flask(__name__)
 
 # Flask-WTF requires an enryption key - the string can be anything
@@ -23,11 +24,41 @@ filename=""
 len_rules=0
 rules=[]
 onlyRules=[]
+my_query=""
+filter_t0=""
+data=""
+api=False
 
 
-@app.route('/')
+@app.route('/',methods=["POST","GET"])
 def settingRules():
-    return render_template('settingRules.html')
+ # Take info from global variable
+ global sleep_value
+ global temporal_window
+ global min_support
+ global min_confidence
+ global data
+ global my_query
+ global filterT0
+ global api
+ 
+ if request.method=="GET":
+  api=False
+  return render_template('settingRules.html') 
+
+ #Read POST request from App API
+ elif request.method=="POST":
+  record=json.loads(request.data)
+  data=record['data']
+  sleep_value=record['sleep_value']
+  temporal_window=record['temporal_window']
+  min_confidence=record['min_confidence']
+  min_support=record['min_support']
+  my_query=record['my_query']
+  filterT0=record['filterT0'] 
+  api=True
+
+  return rulesGeneration() 
  
 @app.route('/rulesGeneration', methods=["POST","GET"])
 def rulesGeneration(): 
@@ -40,13 +71,17 @@ def rulesGeneration():
  global filename
  global rules
  global onlyRules
+ global data
+ global api
  
  if request.method=="POST":
-  data=request.form.get('data_select')
-  sleep_value= request.form.get("sleep_value")
-  temporal_window=request.form.get("temporal_window")
-  min_support=request.form.get("min_support")
-  min_confidence=request.form.get("min_confidence")
+   #Take info from form if there isn't api request
+  if not api:
+   data=request.form.get('data_select')
+   sleep_value= request.form.get("sleep_value")
+   temporal_window=request.form.get("temporal_window")
+   min_support=request.form.get("min_support")
+   min_confidence=request.form.get("min_confidence")
  
   correct=True
 
@@ -93,13 +128,17 @@ def rulesGeneration():
          #take rule structure with specific confidence or support (not all rule)
          _,rules = utilities.__takeRuleImplicit(rules,min_support,min_confidence)
     
-     return render_template("rulesGeneration.html", sleep_value=sleep_value,
-     temporal_window=temporal_window,min_support=min_support,min_confidence=min_confidence,
-     rules=rules,len_rules=len(rules))
+     if not api:
+      return render_template("rulesGeneration.html", sleep_value=sleep_value,
+      temporal_window=temporal_window,min_support=min_support,min_confidence=min_confidence,
+      rules=rules,len_rules=len(rules))
+     else: return matchQueryForm()
   
   # Error in form
   else:
-      return render_template("settingRules.html",message=message)
+      if not api:
+       return render_template("settingRules.html",message=message)
+      else: return json.dumps({'description':message,'typeMatch' : '', 'ruleMatch' :'' })
  
  #GET request
  elif request.method=="GET":
@@ -109,7 +148,10 @@ def rulesGeneration():
 
 @app.route('/matchQueryForm', methods=["POST","GET"])
 def matchQueryForm():
-     return render_template('matchQueryForm.html',temporal_window=temporal_window) 
+ global api
+ if not api:
+  return render_template('matchQueryForm.html',temporal_window=temporal_window) 
+ else: return matchingQuery()
 
 
 @app.route('/matchingQuery', methods=["POST","GET"])
@@ -121,19 +163,25 @@ def matchingQuery():
     global min_confidence
     global onlyRules
     global rules
- 
+    global my_query
+    global filterT0
+    global api
+
     temporal_window=int(temporal_window)
     min_support=float(min_support)
     min_confidence=float(min_confidence)
 
     #obtain info from form
-    my_query=request.form.get('my_query')
-    filterT0=request.form.get('filterT0')
+    if not api:
+     my_query=request.form.get('my_query')
+     filterT0=request.form.get('filterT0')
     
     #Check if form is uncompleted or query is invalid
     value,message = utilities.__isQueryValid(temporal_window,my_query)
     if not value:
-        return render_template("matchQueryForm.html",message=message,temporal_window=temporal_window)
+        if not api:
+         return render_template("matchQueryForm.html",message=message,temporal_window=temporal_window)
+        else: return json.dumps({'description':message,'typeMatch' : '', 'ruleMatch' :'' })
 
     #take only rule with specific confidence or support not all rule
     onlyRules,_ = utilities.__takeRuleImplicit(rules,min_support,min_confidence)
@@ -141,7 +189,11 @@ def matchingQuery():
     #MATCHING QUERY
     typeMatch, ruleMatch=utilities.__findMatching(my_query,onlyRules,filterT0)
 
-    return render_template('matchingQuery.html',my_query=my_query, typeMatch=typeMatch, ruleMatch=ruleMatch) 
+    if not api:
+     return render_template('matchingQuery.html',my_query=my_query, typeMatch=typeMatch, ruleMatch=ruleMatch) 
+    else:
+        result = {'description':'correct','typeMatch' : typeMatch, 'ruleMatch' : ruleMatch}
+        return json.dumps(result)
 
 # keep this as is
 if __name__ == '__main__':
